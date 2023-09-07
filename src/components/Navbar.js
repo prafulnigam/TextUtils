1e341a05b5fd09877c6e50e0e81e987fa768f89d
Praful Nigam <prafulnigam16@gmail.com>
2023-09-07 10:30:42 +0530

    Removed About Us

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,14 +25,14 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
+              {/* <a className="nav-link active" aria-current="page" href="#">
                 Home
-              </a>
+              </a> */}
             </li>
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
+              {/* <a className="nav-link active" aria-current="page" href="#">
                 {props.about}
-              </a>
+              </a> */}
             </li>
           </ul>
           <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light' }`}>
